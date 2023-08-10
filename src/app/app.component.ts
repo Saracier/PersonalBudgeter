e0e262172ddc422a09f5e538147692059fe46393
Saracier <105667230+Saracier@users.noreply.github.com>
2023-08-10 12:02:35 +0200

    feat: add logic of sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'PersonalBudgeter';
+  @ViewChild('snav', { static: true }) snav!: MatSidenav;
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    setInterval(() => console.log(this.mobileQuery), 1000);
+    // setInterval(() => {
+    //   if (this.mobileQuery.matches) {
+    //     console.log(this.snav);
+    //     setInterval(() => this.snav.open(), 1000);
+    //     // this.snav.toggle!();
+    //   }
+    // }, 1000);
+  }
+
+  ngOnInit() {
+    if (!this.mobileQuery.matches) {
+      this.snav.open();
+      // console.log(this.snav);
+      // setInterval(() => this.snav.open(), 1000);
+      // this.snav.toggle();
+    }
+  }
+
+  // ngDoCheck() {
+  //   if (!this.mobileQuery.matches) {
+  //     console.log(this.snav);
+  //     setInterval(() => this.snav.toggle(), 1000);
+  //     // this.snav.toggle();
+  //   }
+  // }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
